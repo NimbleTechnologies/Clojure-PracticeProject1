1265ca8c33c1196c49d859bd5132062c671ac8ed
ttallman <dspiteself@gmail.com>
2012-09-08 15:33:36 -0500

    add main to project clj

--- Sample/project.clj
+++ Sample/project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :main "Sample.core"
   :dependencies [
                  [org.clojure/clojure "1.4.0"]
                  [org.clojure/data.csv "0.1.2"]
