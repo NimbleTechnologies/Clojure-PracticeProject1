212f3db714bcd791dd894c2c61e953b3a6e1a4f3
sitangud <siva.kumar@nimbletechnologies.net>
2012-09-09 01:05:07 +0530

    changed dependency on clojure 1.4.0 creating and populating concept schema and data

--- Sample/project.clj
+++ Sample/project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
-                 [org.clojure/clojure "1.3.0"]
+                 [org.clojure/clojure "1.4.0"]
                  [org.clojure/data.csv "0.1.2"]
                  [com.datomic/datomic-free "0.8.3397"]])
 
--- Sample/src/Sample/core.clj
+++ Sample/src/Sample/core.clj
@@ -1,8 +1,7 @@
 (ns Sample.core)
 
 (require '[clojure.data.csv :as csv]
-         '[clojure.java.io :as io]
-         '[datomic.api :as d])
+         '[clojure.java.io :as io])
 
 (defn -main
   "I don't do a whole lot."
@@ -15,16 +14,32 @@
 (use 'clojure.pprint)
 
 (def uri "datomic:free://localhost:4334//hdd")
+
+(d/delete-database uri)
 (d/create-database uri)
+
 (def conn (d/connect uri))
 
 (def schema-tx (read-string (slurp "data/schema.dtm")))
 (println "schema-tx:")
-(pprint schema-tx)
 
 @(d/transact conn schema-tx)
 
-(defn nreader [filename]
-  (with-open [in-file (io/reader filename)]
-    (doall
-      (csv/read-csv in-file))))
+(println "loading data..");
+
+(with-open [in-file (io/reader "data/concept.csv")]
+  (doall
+    (let [records (csv/read-csv in-file)]
+      (for [onerecord records]
+        (d/transact conn 
+          [{:concept/ncid (nth onerecord 0),
+            :concept/cid (nth onerecord 1),
+            :concept/status_ncid (nth onerecord 2),
+            :concept/superceded_by_ncid (nth onerecord 3),
+            :concept/enterprise_ncid (nth onerecord 4),
+            :concept/concept_definition (nth onerecord 5),
+            :concept/comments (nth onerecord 6),
+            :concept/schema_ncid (nth onerecord 7),
+            :db/id #db/id[:db.part/db -1000003]}])))))
+
+(println "done loading..");
